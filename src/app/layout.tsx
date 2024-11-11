2a1aa4319f4fc8df259133ef4897c864a7fb35f1
sinaGh-cyber <mohammad.sina.gholami.cyber@gmail.com>
2024-11-11 15:33:41 +0330

    course comments api handled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,72 +1,74 @@
-import "./globals.css";
+import './globals.css';
 
-import { Figtree } from "next/font/google";
-import localFont from "next/font/local";
-import { Header } from "./_components/header";
-import { Footer } from "./_components/footer";
-
+import { Figtree } from 'next/font/google';
+import localFont from 'next/font/local';
+import { Header } from './_components/header';
+import { Footer } from './_components/footer';
+import QueryProvider from '@/providers/react-query-provider';
+import { queryClient } from '@/lib/react-query';
 
 const figtree = Figtree({
-  display: "swap",
-  subsets: ["latin"],
-  variable: "--font-figtree",
-  weight: ["300", "400", "500", "600", "700", "700", "800", "900"],
+	display: 'swap',
+	subsets: ['latin'],
+	variable: '--font-figtree',
+	weight: ['300', '400', '500', '600', '700', '700', '800', '900'],
 });
 
 const yekanbakh = localFont({
-  src: [
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Thin.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-SemiBold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Black.woff2",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  variable: "--font-yekanbakh",
+	src: [
+		{
+			path: '../../public/fonts/yekanbakh/YekanBakhFaNum-Thin.woff2',
+			weight: '100',
+			style: 'normal',
+		},
+		{
+			path: '../../public/fonts/yekanbakh/YekanBakhFaNum-Light.woff2',
+			weight: '300',
+			style: 'normal',
+		},
+		{
+			path: '../../public/fonts/yekanbakh/YekanBakhFaNum-Regular.woff2',
+			weight: '400',
+			style: 'normal',
+		},
+		{
+			path: '../../public/fonts/yekanbakh/YekanBakhFaNum-SemiBold.woff2',
+			weight: '600',
+			style: 'normal',
+		},
+		{
+			path: '../../public/fonts/yekanbakh/YekanBakhFaNum-Bold.woff2',
+			weight: '700',
+			style: 'normal',
+		},
+		{
+			path: '../../public/fonts/yekanbakh/YekanBakhFaNum-Black.woff2',
+			weight: '900',
+			style: 'normal',
+		},
+	],
+	variable: '--font-yekanbakh',
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  console.log(yekanbakh.variable);
-  
-  return (
-    <html
-      dir="rtl"
-      className={`${yekanbakh.variable} ${figtree.variable} dark`}>
-      <body className="min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
-        <Header />
-        <main>
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
-  );
+	console.log(yekanbakh.variable);
+
+	return (
+		<html
+			dir="rtl"
+			className={`${yekanbakh.variable} ${figtree.variable} dark`}
+		>
+			<body className="min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
+				<QueryProvider>
+					<Header />
+					<main>{children}</main>
+					<Footer />
+				</QueryProvider>
+			</body>
+		</html>
+	);
 }
